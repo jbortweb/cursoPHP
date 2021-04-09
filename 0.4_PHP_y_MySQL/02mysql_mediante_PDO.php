ff9cd474bc538f47caef9f3ae681d9968b656707
jbortweb <jbortweb@gmail.com>
2021-04-09 10:58:19 +0200

    confirm

--- 0.4_PHP_y_MySQL/02mysql_mediante_PDO.php
+++ 0.4_PHP_y_MySQL/02mysql_mediante_PDO.php
@@ -3,6 +3,9 @@
 try {
 
     $conexion = new PDO('mysql:host=localhost;dbname=prueba_consola', 'root', '');
+    echo "Conexion ok";
+
+    //Se trabaja con los datos dentro de la funcion para comprobar errores.
 
 }catch(PDOException $e){
 
